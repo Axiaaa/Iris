b2974bc2d738cd9568ec2155d695a7588a457a1f
Nephtys <lcamerly@student.42lyon.fr>
2023-12-07 21:42:22 +0100

    Deleted changelog.py from Pipo, added a new one Improved info.py Added botversion const in const.py

--- extensions/info.py
+++ extensions/info.py
@@ -16,10 +16,11 @@ class Info(Extension):
             try : 
                 embed = Embed(title="Informations du bot", color=Color.from_hex("#2596be"), timestamp=Timestamp.now())
                 embed.add_field(name="Développé en", value="Python, avec [interactions.py (v5)](https://github.com/interactions-py/interactions.py)", inline=True)
-                embed.add_field(name="Créé le", value=f"<t:1685136462:F>", inline=False)
-                embed.add_field(name="Créé par", value=self.bot.owner, inline=False)
+                embed.add_field(name="Créé le", value=f"<t:1700768182:F>", inline=False)
+                embed.add_field(name="Créé par", value=f"<@{self.bot.owner.id}>", inline=False)
                 embed.add_field(name="Serveurs", value=len(self.bot.guilds), inline=True)
                 embed.add_field(name="Latence", value=f"{round(self.bot.latency * 100)} ms", inline=True)
+                embed.thumbnail = self.bot.user.avatar_url
                 # embed.add_field("Pour m'inviter sur votre serveur", value = "Cliquez **[ici](https://discord.com/api/oauth2/authorize?client_id=1111756058902409327&permissions=8&scope=bot)**")
                 await ctx.send(embed=embed)
             except OverflowError : 
---
+++ utils/changelog.py
@@ -0,0 +1,32 @@
+from interactions import is_owner, Modal, ModalContext, ParagraphText, ShortText, SlashContext, slash_command,Embed,Permissions, Extension
+from const import BOT_VERSION
+
+
+
+class Changelog(Extension) : 
+
+    is_owner()
+    @slash_command(name="bot_changelog", description="Ajoute un changelog pour le bot")
+    async def bot_changelog(self, ctx: SlashContext):
+        my_modal = Modal(
+        ShortText(label="Version",
+                    custom_id="version",
+                    placeholder=f"{BOT_VERSION}",
+                    required=True),
+
+        ParagraphText(label="Changelog",
+                      custom_id="changelogtext",
+                      placeholder="Entrez les changements",
+                      required=True),
+                      title="Ajouter un changelog")
+    
+        await ctx.send_modal(modal=my_modal)
+        modal_ctx: ModalContext = await ctx.bot.wait_for_modal(my_modal)
+        embed = Embed(title="Nouveau changelog !\t:tada:", description=f"\nChangements apportés par cette mise à jour :\n\n {modal_ctx.responses['changelogtext']}")
+        embed.set_footer(text=f"Iris {modal_ctx[modal_ctx.responses['version']]}", icon_url=self.bot.user.avatar_url)
+        await ctx.channel.send(content= "<@&1182417319557873694>", embed=embed)
+        await ctx.send("Changelog ajouté !", ephemeral=True)
+        await modal_ctx.send("Changelog ajouté !", ephemeral=True)
+        
+def setup(bot): 
+    Changelog(bot)
